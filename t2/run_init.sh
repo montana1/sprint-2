627ac087e215c8d0ed897b694bacd1e30f89a975
montana1 <said001@list.ru>
2024-12-20 02:19:45 +0300

    add app docker and edit init.sh

--- t2/run_init.sh
+++ t2/run_init.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
+docker-compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
 rs.initiate({_id : "config_server",configsvr: true, members: [{ _id : 0, host : "configSrv:27017" }]});
 exit();
 EOF
 
-sudo docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+docker-compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
 rs.initiate({_id : "shard1",members: [{ _id : 0, host : "shard1:27018" },]});
 exit();
 EOF
 
-sudo docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+docker-compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
 rs.initiate({_id : "shard2",members: [{ _id : 0, host : "shard2:27019" },]});
 exit();
 EOF
 
-sudo docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
+docker-compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
 sh.addShard( "shard1/shard1:27018");
 sh.addShard( "shard2/shard2:27019");
 sh.enableSharding("somedb");
